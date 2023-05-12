85d53a75fd27904a2293e2dd8674cd1ca04b19f9
Sofyan289 <114239253+Sofyan289@users.noreply.github.com>
2023-05-12 13:55:13 +0200

    sturen

---
+++ post.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="Post" action="post.php">
+        <input type="text" name="naam" placeholder="Naam?"><br>
+        <input type="text" name="achternaam" placeholder="achternaam?"><br>
+        <input type="number" name="leeftijd" placeholder="Je leeftijd in cijfers?"><br>
+        <input type="adres" name="adres" placeholder="adres"><br>
+        <input type="email" name="email" placeholder="voorbeeld@email.com"><br>
+        <input type="submit" value="Verstuur"><br>
+    </form>
+</body>
+</html>
+
+<?php
+
+$naam = $_POST["naam"];
+$achternaam = $_POST["achternaam"];
+$leeftijd = $_POST["leeftijd"];
+$adres = $_POST["adres"];
+$email = $_POST["email"];
+
+echo $naam . PHP_EOL;
+echo $achternaam . PHP_EOL;
+echo $leeftijd . PHP_EOL;
+echo $adres . PHP_EOL;
+echo $email . PHP_EOL;
+?>
