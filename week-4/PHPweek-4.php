47f6f53402e614f952353e8fefec1b21c9568c63
Sofyan289 <114239253+Sofyan289@users.noreply.github.com>
2023-05-26 14:14:01 +0200

    stuur

---
+++ week-4/PHPweek-4.php
@@ -0,0 +1,35 @@
+<?php
+// opdracht 1
+// for($i = 0; $i <= 50; $i++) {
+//     echo $i . PHP_EOL;
+// }
+
+// opdracht 2
+
+// $klas = ['Sofyan', 'Mohamed', 'Mous', 'Appie',
+// 'Jan', 'dirk', 'Marwan', 'Anouar', 'Durmus', 'Dennis'];
+
+// echo $klas[0] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[1] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[2] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[3] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[4] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[5] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[6] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[7] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[8] . " zit in mijn klas" . PHP_EOL;
+// echo $klas[9] . " zit in mijn klas" . PHP_EOL;
+
+// opdracht 3
+
+// $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
+
+// $j = 1;
+// for($i = 0; $i <= count($maanden)-1; $i++, $j++) {
+// echo "Maand " . $j . " is " .$maanden[$i] . PHP_EOL;
+// }
+
+// foreach ($maanden as $value) {
+//     echo "$value" . PHP_EOL;
+// }
+?>
