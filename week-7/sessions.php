d2670510e0f074af3fa8f2b1de8fd17d7b4e237a
Sofyan289 <114239253+Sofyan289@users.noreply.github.com>
2023-06-17 00:22:21 +0200

    stuuur

--- week-7/sessions.php
+++ week-7/sessions.php
@@ -1,17 +1,45 @@
 <?php
-// Start the session
 session_start();
+?>
+<?php
+$host = 'localhost:3306';
+$db   = 'product';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} 
+catch (\PDOException $e) 
+{
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 ?>
 <!DOCTYPE html>
 <html>
 <body>
 
+<h1>alle data uit mijn tabel </h1><br>
 <?php
-// Set session variables
-$_SESSION["naam"] = "Sofyan";
-$_SESSION["email"] = "Sofyan289@gmail.com";
-echo "Session variables are set.";
+    $namen = array('');
+    $data = $pdo->query("SELECT * FROM winkel")->fetchAll();
+    foreach ($data as $row) {
+        echo $row['product_naam']."<br />\n";
+        array_push($namen, $row['product_naam']);
+    }
+
+    $_SESSION['namen'] = $namen;
+    var_dump($_SESSION);
 ?>
+
 
 </body>
 </html>
